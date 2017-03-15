190e36845a16e081b6e689a6d5f56eef92459943
Derryn <derryn@punk.digital>
2017-03-15 10:56:10 +0200

    Created a new project and changed original template to include a nav bar and content layout

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  	<navigation />
+  	<div class="content">
+  		Content
+  	</div>
+  </div>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+
+export default {
+  name: 'app',
+  components: { Navigation },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+	body {
+		margin: 0px;
+	}
+	.content {
+		margin: 20px 100px;
+	}
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="headerContainer">
+		<h3 class="logo">Beach-Inspector</h3>
+	</div>	
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+	.headerContainer 
+	{
+		font-family: 'Roboto', sans-serif;
+		background : #0097d9;
+		color: #fff;
+		width: 100%;
+		height: 50px;
+		display: block;
+		margin: auto;
+		top: 0;
+		left: 0;
+	}
+	.logo 
+	{
+		display: inline-block;
+		margin: 10px 0 0 10px;
+	}
+</style>
